8fcdb7c0e800a02f6135d19d037e7a1442b5224e
Jianfeng Sun <sunjianfeng7898@gmail.com>
2026-07-21 13:23:52 -0600

    chore: migrate WangChuan to UE5.8

--- Source/WangChuan.Target.cs
+++ Source/WangChuan.Target.cs
@@ -5,11 +5,23 @@ using System.Collections.Generic;
 
 public class WangChuanTarget : TargetRules
 {
-	public WangChuanTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
-		ExtraModuleNames.Add("WangChuan");
-	}
+    public WangChuanTarget(TargetInfo Target)
+        : base(Target)
+    {
+        Type = TargetType.Game;
+
+        DefaultBuildSettings =
+            BuildSettingsVersion.V7;
+
+        IncludeOrderVersion =
+            EngineIncludeOrderVersion.Unreal5_8;
+
+        ExtraModuleNames.AddRange(
+            new string[]
+            {
+                "WangChuan"
+            }
+        );
+    }
 }
+
--- Source/WangChuanEditor.Target.cs
+++ Source/WangChuanEditor.Target.cs
@@ -5,11 +5,22 @@ using System.Collections.Generic;
 
 public class WangChuanEditorTarget : TargetRules
 {
-	public WangChuanEditorTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
-		ExtraModuleNames.Add("WangChuan");
-	}
+    public WangChuanEditorTarget(TargetInfo Target)
+        : base(Target)
+    {
+        Type = TargetType.Editor;
+
+        DefaultBuildSettings =
+            BuildSettingsVersion.V7;
+
+        IncludeOrderVersion =
+            EngineIncludeOrderVersion.Unreal5_8;
+
+        ExtraModuleNames.AddRange(
+            new string[]
+            {
+                "WangChuan"
+            }
+        );
+    }
 }
